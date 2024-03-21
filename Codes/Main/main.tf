3a18587f4576a9b5b282a5f7e50d5c326df8a506
Ali Fırat Çobanoğlu <155168172+AEuphrates@users.noreply.github.com>
2024-03-21 23:48:24 +0300

    Update main.tf

--- Codes/Main/main.tf
+++ Codes/Main/main.tf
@@ -284,3 +284,26 @@ resource "google_container_cluster" "gke_cluster" {
     }
   }
 }
+
+resource "google_service_account" "readonly_user" {
+  account_id   = "readonly-user"
+  display_name = "Read Only User"
+}
+
+resource "google_project_iam_binding" "project_viewer" {
+  project = var.project_id
+  role    = "roles/viewer"
+
+  members = [
+    "serviceAccount:${google_service_account.readonly_user.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "compute_instance_creator" {
+  project = var.project_id
+  role    = "roles/compute.instanceAdmin.v1"
+
+  members = [
+    "serviceAccount:${google_service_account.readonly_user.email}",
+  ]
+}
